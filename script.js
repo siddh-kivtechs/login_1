0bc564825393f31f7f01647dba886d23396bad1f
siddh-kivtechs <siddhbose@kneoginigmisarch.co.in>
2023-09-23 17:42:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,3 +24,39 @@ const showHiddenPass = (loginPass, loginEye) => {
 };
 
 showHiddenPass("login-pass", "login-eye");
+
+/*=============== POST LOGIN DATA ===============*/
+const loginForm = document.querySelector(".login__form");
+
+loginForm.addEventListener("submit", (e) => {
+	e.preventDefault(); // Prevent form submission
+
+	// Get the values from the form inputs
+	const email = document.querySelector(".login__input[type='email']").value;
+	const password = document.querySelector(".login__input[type='password']").value;
+
+	// Create an object with the login data
+	const loginData = {
+		email: email,
+		password: password
+	};
+
+	// Post the login data to the endpoint
+	fetch("https://kivtechs.cloud/", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify(loginData)
+	})
+		.then(response => response.json())
+		.then(data => {
+			// Handle the response from the server
+			console.log(data);
+			// Redirect to a new page or perform other actions based on the response
+		})
+		.catch(error => {
+			// Handle any errors that occur during the request
+			console.error(error);
+		});
+});
