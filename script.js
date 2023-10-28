cd95a27c751e7430a00e3cbf281b4fb56af36792
siddh-kivtechs <siddhbose@kneoginigmisarch.co.in>
2023-10-29 00:46:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 /*=============== SHOW HIDDEN - PASSWORD ===============*/  
 const showHiddenPass = (loginPass, loginEye) => {  
-	const input = document.getElementById(loginPass),  
-		iconEye = document.getElementById(loginEye);  
+  const input = document.getElementById(loginPass),  
+    iconEye = document.getElementById(loginEye);  
   
-	iconEye.addEventListener("click", () => {  
-		// Change password to text  
-		if (input.type === "password") {  
-			// Switch to text  
-			input.type = "text";  
+  iconEye.addEventListener("click", () => {  
+    // Change password to text  
+    if (input.type === "password") {  
+      // Switch to text  
+      input.type = "text";  
   
-			// Icon change  
-			iconEye.classList.add("ri-eye-line");  
-			iconEye.classList.remove("ri-eye-off-line");  
-		} else {  
-			// Change to password  
-			input.type = "password";  
+      // Icon change  
+      iconEye.classList.add("ri-eye-line");  
+      iconEye.classList.remove("ri-eye-off-line");  
+    } else {  
+      // Change to password  
+      input.type = "password";  
   
-			// Icon change  
-			iconEye.classList.remove("ri-eye-line");  
-			iconEye.classList.add("ri-eye-off-line");  
-		}  
-	});  
+      // Icon change  
+      iconEye.classList.remove("ri-eye-line");  
+      iconEye.classList.add("ri-eye-off-line");  
+    }  
+  });  
 };  
   
 showHiddenPass("login-pass", "login-eye");  
@@ -29,52 +29,86 @@ showHiddenPass("login-pass", "login-eye");
 const loginForm = document.querySelector(".login__form");  
   
 loginForm.addEventListener("submit", async (e) => {  
-	e.preventDefault(); // Prevent form submission  
+  e.preventDefault(); // Prevent form submission  
   
-	// Get the values from the form inputs  
-	const email = document.querySelector(".login__input[type='email']").value;  
-	const password = document.querySelector(".login__input[type='password']").value;  
-	const emailHash = btoa(email);  
-	const passwordHash = await sha256(password);  
+  // Get the values from the form inputs  
+  const emailInput = document.querySelector(".login__input[type='email']");  
+  const passwordInput = document.querySelector(".login__input[type='password']");  
+  const email = emailInput.value.trim(); // Trim whitespace from email input  
+  const password = passwordInput.value.trim(); // Trim whitespace from password input  
   
-	// Create an object with the login data  
-	const loginData = {  
-		email: emailHash,  
-		password: passwordHash  
-	};  
-	console.log(loginData);  
+  // Validate email and password  
+  if (!validateEmail(email)) {  
+    // Display error message for invalid email  
+    displayErrorMessage(emailInput, "Invalid email");  
+    return; // Stop form submission  
+  }  
   
-	// Post the login data to the TEST endpoint  
-	fetch("https://py-vercel-sage.vercel.app/auth/", {  
-		method: "POST",  
-		headers: {  
-			"Content-Type": "application/json"  
-		},  
-		body: JSON.stringify(loginData)  
-	})  
-		.then(response => response.json())  
-		.then(data => {  
-			// Handle the response from the server  
-			console.log(data);  
-			// Redirect to a new page or perform other actions based on the response  
-		})  
-		.catch(error => {  
-			// Handle any errors that occur during the request  
-			console.error(error);  
-		});  
+  if (!validatePassword(password)) {  
+    // Display error message for invalid password  
+    displayErrorMessage(passwordInput, "Invalid password");  
+    return; // Stop form submission  
+  }  
+  
+  // Hash the email and password  
+  const emailHash = btoa(email);  
+  const passwordHash = await sha256(password);  
+  
+  // Create an object with the login data  
+  const loginData = {  
+    email: emailHash,  
+    password: passwordHash  
+  };  
+  console.log(loginData);  
+  
+  // Post the login data to the TEST endpoint  
+  fetch("https://py-vercel-sage.vercel.app/auth/", {  
+    method: "POST",  
+    headers: {  
+      "Content-Type": "application/json"  
+    },  
+    body: JSON.stringify(loginData)  
+  })  
+    .then(response => response.json())  
+    .then(data => {  
+      // Handle the response from the server  
+      console.log(data);  
+      // Redirect to a new page or perform other actions based on the response  
+    })  
+    .catch(error => {  
+      // Handle any errors that occur during the request  
+      console.error(error);  
+    });  
 });  
   
-async function sha256(message) {  
-	// encode as UTF-8  
-	const msgBuffer = new TextEncoder().encode(message);  
-  
-	// hash the message  
-	const hashBuffer = await crypto.subtle.digest("SHA-256", msgBuffer);  
-  
-	// convert ArrayBuffer to Array  
-	const hashArray = Array.from(new Uint8Array(hashBuffer));  
-  
-	// convert bytes to hex string  
-	const hashHex = hashArray.map((b) => ("00" + b.toString(16)).slice(-2)).join("");  
-	return hashHex;  
+function validateEmail(email) {  
+  // Use regular expression to validate email format  
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;  
+  return emailRegex.test(email);  
 }  
+  
+function validatePassword(password) {  
+  // Check if password is at least 8 characters long  
+  return password.length >= 8;  
+}  
+  
+function displayErrorMessage(inputElement, message) {  
+  // Display error message below the input element  
+  const errorMessage = document.createElement("div");  
+  errorMessage.className = "error-message";  
+  errorMessage.innerText = message;  
+  inputElement.parentNode.appendChild(errorMessage);  
+}  
+  
+async function sha256(message) {  
+  // encode as UTF-8  
+  const msgBuffer = new TextEncoder().encode(message);  
+  
+  // hash the message  
+  const hashBuffer = await crypto.subtle.digest("SHA-256", msgBuffer);  
+  
+  // convert ArrayBuffer to Array  
+  const hashArray = Array.from(new Uint8Array(hashBuffer));  
+  
+  // convert bytes to hex string  
+  const hashHex = hashArray.map((b) => ("00" + b.toString(16)).slice(-
